0fe6041f6f9879670b807c681ce068cb55d39371
Masahiro Nagano <kazeburo@gmail.com>
2019-03-22 16:08:09 +0900

    introduced timeout

--- Makefile
+++ Makefile
@@ -1,4 +1,5 @@
-VERSION=0.0.1
+VERSION=0.0.4
+LDFLAGS=-ldflags "-X main.Version=${VERSION}"
 
 all: check-mysql-slave-sql-error
 
@@ -7,23 +8,23 @@ all: check-mysql-slave-sql-error
 bundle:
 	dep ensure
 
+update:
+	dep ensure -update
+
 check-mysql-slave-sql-error: check-mysql-slave-sql-error.go
-	go build -o check-mysql-slave-sql-error
+	go build $(LDFLAGS) -o check-mysql-slave-sql-error
 
 linux: check-mysql-slave-sql-error.go
-	GOOS=linux GOARCH=amd64 go build -o check-mysql-slave-sql-error
+	GOOS=linux GOARCH=amd64 go build $(LDFLAGS) -o check-mysql-slave-sql-error
+
+check:
+	go test ./...
 
 fmt:
 	go fmt ./...
-
-dist:
-	git archive --format tgz HEAD -o check-mysql-slave-sql-error-$(VERSION).tar.gz --prefix check-mysql-slave-sql-error-$(VERSION)/
-
-clean:
-	rm -rf check-mysql-slave-sql-error check-mysql-slave-sql-error-*.tar.gz
 
 tag:
 	git tag v${VERSION}
 	git push origin v${VERSION}
 	git push origin master
-
+	goreleaser --rm-dist
--- check-mysql-slave-sql-error.go
+++ check-mysql-slave-sql-error.go
@@ -1,20 +1,24 @@
 package main
 
 import (
+	"context"
+	"database/sql"
 	"fmt"
+	"os"
 	"strings"
+	"time"
+
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/jessevdk/go-flags"
 	"github.com/mackerelio/checkers"
-	"github.com/ziutek/mymysql/mysql"
-	_ "github.com/ziutek/mymysql/native"
-	"os"
 )
 
 type mysqlSetting struct {
-	Host string `short:"H" long:"host" default:"localhost" description:"Hostname"`
-	Port string `short:"p" long:"port" default:"3306" description:"Port"`
-	User string `short:"u" long:"user" default:"root" description:"Username"`
-	Pass string `short:"P" long:"password" default:"" description:"Password"`
+	Host    string        `short:"H" long:"host" default:"localhost" description:"Hostname"`
+	Port    string        `short:"p" long:"port" default:"3306" description:"Port"`
+	User    string        `short:"u" long:"user" default:"root" description:"Username"`
+	Pass    string        `short:"P" long:"password" default:"" description:"Password"`
+	Timeout time.Duration `long:"timeout" default:"10s" description:"Timeout to connect mysql"`
 }
 
 type connectionOpts struct {
@@ -35,29 +39,78 @@ func checkSlaveSQLerror() *checkers.Checker {
 		os.Exit(1)
 	}
 
-	db := mysql.New("tcp", "", fmt.Sprintf("%s:%s", opts.Host, opts.Port), opts.User, opts.Pass, "")
-	err = db.Connect()
+	db, err := sql.Open(
+		"mysql",
+		fmt.Sprintf(
+			"%s:%s@tcp(%s:%s)/",
+			opts.mysqlSetting.User,
+			opts.mysqlSetting.Pass,
+			opts.mysqlSetting.Host,
+			opts.mysqlSetting.Port,
+		),
+	)
 	if err != nil {
-		return checkers.Critical("couldn't connect DB")
+		return checkers.Critical(fmt.Sprintf("couldn't connect DB: %v", err))
 	}
 	defer db.Close()
 
-	rows, res, err := db.Query("SHOW SLAVE STATUS")
-	if err != nil {
-		return checkers.Unknown("couldn't execute query")
-	}
+	ctx, cancel := context.WithTimeout(context.Background(), opts.Timeout)
+	defer cancel()
+	ch := make(chan error, 1)
 
 	var lastSQLErrors []string
-	idxLastSQLError := res.Map("Last_SQL_Error")
-	for _, row := range rows {
-		lastSQLError := row.Str(idxLastSQLError)
-		if lastSQLError != "" {
-			lastSQLErrors = append(lastSQLErrors, lastSQLError)
+	go func() {
+		rows, e := db.Query("SHOW SLAVE STATUS")
+		if e != nil {
+			ch <- e
+			return
 		}
+		defer rows.Close()
+		cols, e := rows.Columns()
+		if e != nil {
+			ch <- e
+			return
+		}
+		vals := make([]interface{}, len(cols))
+		idxLastSQLError := -1
+		for i, v := range cols {
+			vals[i] = new(sql.RawBytes)
+			if v == "Last_SQL_Error" {
+				idxLastSQLError = i
+			}
+		}
+		if idxLastSQLError < 0 {
+			ch <- fmt.Errorf("Could not find Last_SQL_Error in columns")
+			return
+		}
+		for rows.Next() {
+			e = rows.Scan(vals...)
+			if e != nil {
+				ch <- e
+				return
+			}
+			lastSQLError := string(*vals[idxLastSQLError].(*sql.RawBytes))
+			if lastSQLError != "" {
+				lastSQLErrors = append(lastSQLErrors, lastSQLError)
+			}
+		}
+		ch <- nil
+	}()
+
+	select {
+	case err = <-ch:
+		// nothing
+	case <-ctx.Done():
+		err = fmt.Errorf("connection or query timeout")
 	}
+
+	if err != nil {
+		return checkers.Critical(fmt.Sprintf("Couldn't fetch slave status: %v", err))
+	}
+
 	if len(lastSQLErrors) > 0 {
-		msg := strings.Join(lastSQLErrors[0:]," | ")
-		return checkers.Critical("Last_SQL_Error found: "+msg)
+		msg := strings.Join(lastSQLErrors[0:], " | ")
+		return checkers.Critical("Last_SQL_Error found: " + msg)
 	}
 	return checkers.Ok("No Last_SQL_Error exists")
 
